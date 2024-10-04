e93d0e9f86ac62cee79621fc4cc803d68cc332a6
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-10-04 15:38:30 +0100

    refactors getTotalAmount for optimisation

--- src/main/java/uk/gov/dwp/uc/pairtest/service/PayableAmountCalculator.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/service/PayableAmountCalculator.java
@@ -3,27 +3,25 @@ package uk.gov.dwp.uc.pairtest.service;
 import uk.gov.dwp.uc.pairtest.data.PriceData;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class PayableAmountCalculator implements AmountCalculatorService {
+    private final Map<String, Integer> ticketPrices = PriceData.getTicketPrices();
+
     @Override
     public int getTotalAmountToPay(TicketTypeRequest... ticketTypeRequests) {
         if (ticketTypeRequests == null) {
             throw new IllegalArgumentException("Ticket requests cannot be null");
         }
-        int totalAmount = 0;
-        for (TicketTypeRequest request : ticketTypeRequests) {
-            if (TicketTypeRequest.Type.ADULT == request.getTicketType()) {
-                totalAmount = getTotalAmount(request, totalAmount);
-            } else if (TicketTypeRequest.Type.CHILD == request.getTicketType()) {
-                totalAmount = getTotalAmount(request, totalAmount);
-            } else if (TicketTypeRequest.Type.INFANT == request.getTicketType()) {
-                totalAmount = getTotalAmount(request, totalAmount);
-            }
-        }
-        return totalAmount;
+
+        return Arrays.stream(ticketTypeRequests)
+                .filter(request -> request.getNoOfTickets() > 0) // Only consider requests with tickets
+                .mapToInt(this::calculateAmount) // Map each request to its total amount
+                .sum(); // Sum all amounts
     }
 
-    private static int getTotalAmount(TicketTypeRequest request, int totalAmount) {
-        totalAmount += PriceData.getTicketPrices().get(request.getTicketType().name()) * request.getNoOfTickets();
-        return totalAmount;
+    private int calculateAmount(TicketTypeRequest request) {
+        return ticketPrices.getOrDefault(request.getTicketType().name(), 0) * request.getNoOfTickets();
     }
 }
