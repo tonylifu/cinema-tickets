b0021e680c201e4fb9815df550e0b141c0f14414
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-10-05 00:42:16 +0100

    refactors seats number calculator

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/exception/ExceptionMessages.java
@@ -0,0 +1,7 @@
+package uk.gov.dwp.uc.pairtest.exception;
+
+public class ExceptionMessages {
+    private ExceptionMessages(){}
+
+    public static final String NULL_TICKET_REQUEST_MESSAGE = "Ticket requests cannot be null.";
+}
