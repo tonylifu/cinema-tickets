1288f66bf51831bb5027f25abf8ee20ef3d337fe
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-10-04 12:43:53 +0100

    optimises get number of seats method using stream api

--- src/main/java/uk/gov/dwp/uc/pairtest/service/NumberOfSeatsCalculator.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/service/NumberOfSeatsCalculator.java
@@ -2,16 +2,15 @@ package uk.gov.dwp.uc.pairtest.service;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
+import java.util.stream.Stream;
+
 public class NumberOfSeatsCalculator implements SeatsCalculatorService {
     @Override
     public int getNumberOfSeatsToAllocate(TicketTypeRequest... ticketTypeRequests) {
-        int totalSeats = 0;
-        for (TicketTypeRequest request : ticketTypeRequests) {
-            if (request.getTicketType() == TicketTypeRequest.Type.ADULT ||
-                    request.getTicketType() == TicketTypeRequest.Type.CHILD) {
-                totalSeats += request.getNoOfTickets();
-            }
-        }
-        return totalSeats;
+        return Stream.of(ticketTypeRequests)
+                .filter(request -> request.getTicketType() == TicketTypeRequest.Type.ADULT
+                        || request.getTicketType() == TicketTypeRequest.Type.CHILD)
+                .mapToInt(TicketTypeRequest::getNoOfTickets)
+                .sum();
     }
 }
