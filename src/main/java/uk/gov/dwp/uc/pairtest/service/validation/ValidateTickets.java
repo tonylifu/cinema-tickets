e9c4753106657f4f969a27d42780fd78f2203e0b
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-10-05 01:21:03 +0100

    adds javadoc for validation service

--- src/main/java/uk/gov/dwp/uc/pairtest/service/validation/ValidateTickets.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/service/validation/ValidateTickets.java
@@ -4,12 +4,22 @@ import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
 import java.util.Arrays;
 
+import static uk.gov.dwp.uc.pairtest.exception.ExceptionMessages.NULL_TICKET_REQUEST_MESSAGE;
+
 public class ValidateTickets implements ValidationService {
 
     @Override
     public boolean isAdultInTicketsRequest(TicketTypeRequest... ticketTypeRequests) {
         if (ticketTypeRequests == null) {
-            throw new IllegalArgumentException("Ticket requests cannot be null");
+            throw new IllegalArgumentException(NULL_TICKET_REQUEST_MESSAGE);
+        }
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            if (request == null) {
+                throw new IllegalArgumentException(NULL_TICKET_REQUEST_MESSAGE);
+            }
+        }
+        if (ticketTypeRequests.length == 0) {
+            return false;
         }
 
         return Arrays.stream(ticketTypeRequests)
--- src/main/java/uk/gov/dwp/uc/pairtest/service/validation/ValidationService.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/service/validation/ValidationService.java
@@ -2,6 +2,23 @@ package uk.gov.dwp.uc.pairtest.service.validation;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
+/**
+ * The {@code ValidationService} interface provides a method to validate whether an
+ * adult ticket is present in the provided ticket type requests.
+ */
 public interface ValidationService {
+
+    /**
+     * Checks whether any of the provided {@link TicketTypeRequest} instances represents
+     * an adult ticket.
+     *
+     * @param ticketTypeRequests the array of {@link TicketTypeRequest} objects representing
+     *                           the types of tickets (e.g., adult, child) being requested.
+     *                           This parameter must not be null or empty.
+     * @return {@code true} if there is at least one adult ticket in the request,
+     *         {@code false} otherwise.
+     * @throws IllegalArgumentException if {@code ticketTypeRequests} is null or contains null elements.
+     */
     boolean isAdultInTicketsRequest(TicketTypeRequest... ticketTypeRequests);
 }
+
